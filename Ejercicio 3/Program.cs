3f3a6c1f209342e534da4cacb002e1ae66b89a42
Adri4 <justinlopezlucas14@gmail.com>
2026-03-17 08:49:18 -0600

    Empezamos con las listas

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,50 @@
+﻿List<Empleado> listaEmpleados = new List<Empleado>();
+
+Console.Write("¿Cuántos empleados registrará? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"\nEmpleado {i + 1}:");
+    Console.Write("Nombre: ");
+    string nom = Console.ReadLine();
+    Console.Write("Puesto: ");
+    string pue = Console.ReadLine();
+    Console.Write("Salario Mensual: ");
+    double sal = double.Parse(Console.ReadLine());
+
+    listaEmpleados.Add(new Empleado(nom, pue, sal));
+}
+
+Console.WriteLine("\n---Reporte de Empleados ---");
+foreach (var emp in listaEmpleados)
+{
+    emp.MostrarInfo();
+}
+
+public class Empleado
+{
+    public string Nombre;
+    public string Puesto;
+    public double SalarioMensual;
+
+    public Empleado(string n, string p, double s)
+    {
+        Nombre = n; Puesto = p; SalarioMensual = s;
+    }
+
+    public double CalcularAnual() => SalarioMensual * 12;
+    public double CalcularBono() => SalarioMensual > 5000 ? SalarioMensual * 0.10 : SalarioMensual * 0.15;
+    public string Clasificar()
+
+    {
+        if (SalarioMensual <= 3000) return "Básico";
+        if (SalarioMensual <= 7000) return "Medio";
+        return "Alto";
+    }
+
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"{Nombre} - {Puesto} | Anual: Q{CalcularAnual():N2} | Bono: Q{CalcularBono():N2} | Rango: {Clasificar()}");
+    }
+}
